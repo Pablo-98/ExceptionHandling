80e11937755bd01bafcd8db3ac02b91a84b3029c
Pablo Hernandez <pablo@Pablos-MacBook-Air.local>
2023-07-24 12:38:27 -0600

    first

---
+++ exception.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class exception {
+    public static void main (String[] args) throws IOException {
+        Scanner input = new Scanner (new File("hi.txt"));
+    }
+}
